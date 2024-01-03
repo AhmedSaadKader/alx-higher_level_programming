885a76e8a1d4fcb6c850375058aca96266d0bbc7
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-03 03:53:48 +0200

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_tebahpla():
+    for i in range(-122, -96):
+        if (abs(i) % 2 != 0):
+            a = abs(i) - 32
+        else:
+            a = abs(i)
+        print(chr(a), end="")
+
+
+print_tebahpla()
