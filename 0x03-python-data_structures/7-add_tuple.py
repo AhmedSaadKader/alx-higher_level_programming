fd8aea884c340258bc84f78b4765019355cafd9f
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-07 07:46:04 +0200

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,10 +8,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         sum_2 = tuple_a[1] + tuple_b[1]
     elif len(tuple_a) == 1:
         sum_1 = tuple_a[0] + tuple_b[0]
-        sum_2 = tuple_b[1] + 0
+        sum_2 = 0 + tuple_b[1]
     elif len(tuple_a) == 0:
-        sum_1 = tuple_a[0] + tuple_b[0]
-        sum_2 = tuple_b[1] + 0
+        sum_1 = 0 + tuple_b[0]
+        sum_2 = 0 + tuple_b[1]
     elif len(tuple_b) == 1:
         sum_1 = tuple_a[0] + tuple_b[0]
         sum_2 = tuple_a[1] + 0
