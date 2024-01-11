ddef9c87a7186a3299fecf813ce018248a304d41
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-11 03:02:35 +0200

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    weight = 0
+    score = 0
+    for t in my_list:
+        weight += t[1]
+        score += t[0] * t[1]
+    return score / weight
