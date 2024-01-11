1f2db6761bca1833d192ab9ffdd1bcd9e381c67b
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-11 02:13:06 +0200

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary != None:
-        return (sorted(a_dictionary.items(), key=lambda x:x[1], reverse=True)[0][0])
+    if not a_dictionary:
+        return None
+    return (sorted(a_dictionary.items(), key=lambda x: x[1],
+                   reverse=True)[0][0])
