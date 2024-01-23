b84fac2738b5131a05116a145825b573c3f55c41
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-23 21:49:15 +0200

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,5 +47,9 @@ class Square:
         else:
             for _ in range(self.__position[1]):
                 print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            for height in range(self.__size):
+                for _ in range(self.__position[0]):
+                    print(" ", end="")
+                for width in range(self.__size):
+                    print("#", end="")
+                print()
