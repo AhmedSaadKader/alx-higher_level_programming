ce140e5d33376a7a4fb650e866bc9f46e6ac0e8d
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-24 12:02:01 +0200

    revert

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as v:
+    except (TypeError, ValueError):
         sys.stderr.write("Exception: {}\n".format(str(v)))
         sys.stderr.flush()
         return False
