ba8fed90ba3cf4f8f728ee5a94a6ba253f40fa88
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-24 11:51:28 +0200

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as v:
+        sys.stderr.write("Exception: {}\n".format(str(v)))
+        sys.stderr.flush()
+        return False
+    else:
+        return True
