20ea57cbf051682307be35d95d7014f3142e4738
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-24 11:58:48 +0200

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,12 +3,14 @@ import sys
 
 
 def safe_print_integer_err(value):
+    if not isinstance(value, int):
+        sys.stderr.write("Exception: {} is not an integer\n".format(value))
+        sys.stderr.flush()
+        return False
     try:
         printed_value = "{:d}".format(value)
+        return True
     except ValueError as v:
         sys.stderr.write("Exception: {}\n".format(str(v)))
         sys.stderr.flush()
         return False
-    else:
-        print(printed_value)
-        return True
