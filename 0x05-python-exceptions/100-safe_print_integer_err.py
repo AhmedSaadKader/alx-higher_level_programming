e7e9cf0955f722dec8096b8ce8a8ffddd6f3f568
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-24 11:59:58 +0200

    revert

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        printed_value = "{:d}".format(value)
+        print("{:d}".format(value))
         return True
     except ValueError as v:
         sys.stderr.write("Exception: {}\n".format(str(v)))
