084207d5f35aa1012c77542b1b10a78a3b0a9473
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-24 12:03:53 +0200

    typerror

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
+    except (TypeError, ValueError) as v:
         sys.stderr.write("Exception: {}\n".format(str(v)))
         sys.stderr.flush()
         return False
