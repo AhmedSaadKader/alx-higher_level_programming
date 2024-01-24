3a58c2bb89a68753f66d785f81e071533c9c673e
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-25 00:09:07 +0200

    102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""contains a class Square that defines a square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
