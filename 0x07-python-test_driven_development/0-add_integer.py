ebe8fb6a7230b97f2cf709d57dfac3bfd2c2e7fb
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-26 21:02:16 +0200

    m

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+"""
+contains add_integer function 
+
+to add integer to 98 or add two integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    add integer to 98 or add two integers
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer") 
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer") 
+    return int(a) + int(b)
