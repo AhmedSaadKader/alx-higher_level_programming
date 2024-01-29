82b46dfac8326246796e7985744fbff4cb35c652
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-29 13:02:25 +0200

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module containing one class Rectangle
+
+This class Rectangle defines a reactangle
+"""
+
+
+class Rectangle:
+    """
+    Defines a rectangle
+    """
+    def __init__(self):
+        pass
