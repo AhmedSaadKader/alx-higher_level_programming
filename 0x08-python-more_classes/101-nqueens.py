1a45f7169668f99ecee15cb2abf4f3f7c8491e6b
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-29 23:39:25 +0200

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -63,10 +63,10 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: nqueens N")
         exit(1)
-    N = int(sys.argv[1])
-    if type(N) is not int:
+    if type(sys.argv[1]) is not int:
         print("N must be a number")
         exit(1)
+    N = int(sys.argv[1])
     if N < 4:
         print("N must be at least 4")
         exit(1)
