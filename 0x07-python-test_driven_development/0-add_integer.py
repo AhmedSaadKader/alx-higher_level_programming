5848eb00744e1322c63954643edce6d16ec14a02
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-01-31 22:26:42 +0200

    tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     """
     add integer to 98 or add two integers
     """
-    if type(a) not in [int, float]:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
