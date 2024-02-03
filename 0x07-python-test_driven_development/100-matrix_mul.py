55452ea26a7b3b65a979d807134798db0b882ed9
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-03 03:48:42 +0200

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -21,37 +21,44 @@ def matrix_mul(m_a, m_b):
     for row in m_b:
         if not isinstance(row, list):
             raise TypeError("m_b must be a list of lists")
+        if not isinstance(row, list):
+            raise TypeError("m_b must be a list of lists")
+        if len(row) != len(m_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
+ 
     col_b = []
     result = []
     for i in range(len(m_b[0])):
         inner_list = []
         for row in m_b:
-            if not isinstance(row, list):
-                raise TypeError("m_b must be a list of lists")
-            inner_list.append(row[i])
+           inner_list.append(row[i])
         col_b.append(inner_list)
 
     for row in m_a:
+
         if not isinstance(row, list):
             raise TypeError("m_a must be a list of lists")
+        if len(row) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")    
+        if not isinstance(row[0], (int, float)):
+            raise TypeError("m_a should contain only integers or floats")
+        if type(row[1]) not in [int, float]:
+            raise TypeError("m_a should contain only integers or floats")
+
         row_first = row[0]
-        if not isinstance(row_first, (int, float)):
-            raise TypeError("m_a should contain only integers or floats")
         row_second = row[1]
-        if type(row_second) not in [int, float]:
-            raise TypeError("m_a should contain only integers or floats")
         inner_result = []
         for col_no in range(len(m_b)):
+
             if type(col_b[col_no][0]) not in [int, float]:
                 raise TypeError("m_b should contain only integers or floats")
+
             result_first = row_first * col_b[col_no][0]
+
             if type(col_b[col_no][1]) not in [int, float]:
                 raise TypeError("m_b should contain only integers or floats")
+
             result_second = row_second * col_b[col_no][1]
             inner_result.append(result_first + result_second)
         result.append(inner_result)
     return (result)
-
-
-if __name__ == "__main__":
-    matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]])
