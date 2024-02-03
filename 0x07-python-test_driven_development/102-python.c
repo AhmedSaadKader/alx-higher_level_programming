9c98a9ecb3522261b03362c48f4e6dae340daee6
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-03 05:08:33 +0200

    102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+#include "Python.h"
+
+/**
+ * print_python_string - prints Python strings
+ * @p: python string
+*/
+
+void print_python_string(PyObject *p)
+{
+	PyUnicodeObject *str;
+	unsigned long int length;
+
+	printf("[.] string object info\n");
+	if (!PyUnicode_Check(p))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	str = (PyUnicodeObject *)p;
+	printf("  type: %s\n", Py_TYPE(str)->tp_name);
+	printf("  length: %ld\n", PyUnicode_GET_LENGTH(str));
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, NULL));
+}
