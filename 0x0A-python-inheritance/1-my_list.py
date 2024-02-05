513a213ca49c89bc09508a169907d266287dea2f
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-05 16:03:14 +0200

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-This module contains my_list function that inherits from list class - 
+This module contains my_list function that inherits from list class -
 public instance method: def print_sorted(self) that prints the list but
 sorted. All the elements of the list will be of type int
 """
 
 
 class MyList(list):
-	"""inherits from list class
-	has public instance method: def print_sorted that
-	prints the list sorted
-	"""
-	def print_sorted(self):
-		print(sorted(self))
+    """inherits from list class
+    has public instance method: def print_sorted that
+    prints the list sorted
+    """
+    def print_sorted(self):
+        print(sorted(self))
