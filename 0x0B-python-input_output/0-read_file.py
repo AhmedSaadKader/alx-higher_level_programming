f62cad3664bc9572677fe68a830551d47c5135e0
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-06 22:27:05 +0200

    1-

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
         filename (str, optional): the filename. Defaults to "".
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
