49bd0cf7b966bd403af2678ce63430aceef51fea
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-06 23:27:33 +0200

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,11 @@
 import sys
 from pathlib import Path
 
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-file_name = "add_item.json"
-my_list = load_from_json_file(file_name)
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, file_name)
+    file_name = "add_item.json"
+    my_list = load_from_json_file(file_name)
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, file_name)
