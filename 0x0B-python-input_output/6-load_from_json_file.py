3a19d2a1ef95931efab471fe196ee123ed375421
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-06 22:42:14 +0200

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,4 +10,4 @@ def load_from_json_file(filename):
         filename (file): file name
     """
     with open(filename, encoding="utf-8") as f:
-        return json.dumps(f.read())
+        return json.loads(f.read())
