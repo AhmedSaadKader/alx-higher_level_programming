bc811fffd679a1f38d1854c578363c6dd66ae1ac
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-06 23:21:43 +0200

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """a script that add all arguments to a python list"""
-import json
 import sys
+from pathlib import Path
 
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_list = load_from_json_file("add_item.json")
+file_name = "add_item.json"
+file_path = Path(file_name)
+my_list = []
+if file_path.exists():
+    my_list = load_from_json_file("add_item.json")
 save_to_json_file(my_list + sys.argv[1:], "add_item.json")
