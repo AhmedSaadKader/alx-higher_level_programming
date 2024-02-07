8477f34ea3cfcf9efc4854cc6402d394f2f28c21
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-07 07:52:35 +0200

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """initialize a new student
+
+        Args:
+            first_name (str): first name
+            last_name (str): first name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of self
+        """
+        return self.__dict__
