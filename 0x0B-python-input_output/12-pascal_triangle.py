8736e72115451217ca64f88e5812577e52030683
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-02-13 22:46:52 +0200

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,10 +6,19 @@ triangle of n
 
 def pascal_triangle(n):
     """returns a list of lists of integers representing the Pascal's 
-triangle of n
+    triangle of n
 
     Args:
         n (int): number
     """
-
-    
+    if n <= 0:
+        return []
+    pascal_list = [[1]]
+    for _ in range(1, n):
+        prev_row = pascal_list[-1]
+        new_row = [1]
+        for i in range(1, len(prev_row)):
+            new_row.append(prev_row[i-1] + prev_row[i])
+        new_row.append(1)
+        pascal_list.append(new_row)
+    return pascal_list
