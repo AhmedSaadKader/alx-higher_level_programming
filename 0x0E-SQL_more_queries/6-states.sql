ca6c9cab4a58f78e4f03d73f1768b8be39c09567
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-03-14 00:01:11 +0200

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states
+-- create the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create the table
 CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
