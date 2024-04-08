820729ed45f0693a9d4e4cd9fde9692e4107b3de
Ahmed Saad Abd El-Kader <ahmed.saad.kader@gmail.com>
2024-04-08 14:24:08 +0200

    .\10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const num = parseInt(argv[2]);
+const factorial = (num) => {
+  if (isNaN(num)) return 1;
+  if (num == 1) return 1;
+  return num * factorial(num - 1);
+};
+console.log(factorial(num));
