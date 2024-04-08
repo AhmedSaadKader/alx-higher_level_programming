f11c2d510350ebde91369bcf18c7a5b5cdc65777
Ahmed Saad Abd El-Kader <ahmed.saad.kader@gmail.com>
2024-04-08 13:18:14 +0200

    6-

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let index = 0; index < languages.length; index++) {
   const element = languages[index];
   console.log(element);
