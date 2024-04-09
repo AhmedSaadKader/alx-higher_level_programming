8322355508816380f2348078f30c697f45a013a2
Ahmed Saad Abd El-Kader <ahmed.saad.kader@gmail.com>
2024-04-09 23:51:04 +0200

    .\100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 (() => {
-  const new_list = list.map((value, index) => {
+  const newList = list.map((value, index) => {
     return value * index;
   });
   console.log(list);
-  console.log(new_list);
+  console.log(newList);
 })();
