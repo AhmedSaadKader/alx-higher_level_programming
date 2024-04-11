a97ef2dda9026727d3de892b7a2615160e727bde
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-04-11 22:23:37 +0200

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """ This script lists all states from the database hbtn_0e_0_usa
 """
-
 import sys
 import MySQLdb
 
-username = sys.argv[1]
-password = sys.argv[2]
-databaseName = sys.argv[3]
 
-db = MySQLdb.connect(
-    host='localhost', user=username, passwd=password, db=databaseName
-    )
-cur = db.cursor()
-try:
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-except MySQLdb.Error as e:
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    databaseName = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost', user=username, passwd=password, db=databaseName
+        )
+    cur = db.cursor()
     try:
-        print(f"MySQL Error [{e.args[0]}]: {e.args[1]}")
-    except IndexError:
-        print(f"MySQL Error: {str(e)}")
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+        cur.execute("SELECT * FROM states")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print(f"MySQL Error [{e.args[0]}]: {e.args[1]}")
+        except IndexError:
+            print(f"MySQL Error: {str(e)}")
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
