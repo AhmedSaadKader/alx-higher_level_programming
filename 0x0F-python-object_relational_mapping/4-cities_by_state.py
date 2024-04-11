d4196396180a0d96db6dbf6ca0d6bf1342022acd
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-04-11 23:50:21 +0200

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This script lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    databaseName = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost', user=username, passwd=password, db=databaseName
+        )
+    cur = db.cursor()
+    try:
+        cur.execute("""SELECT c.id, c.name, s.name FROM cities AS c
+                    INNER JOIN states AS s
+                    ON state_id = s.id
+                    ORDER BY id""")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print(f"MySQL Error [{e.args[0]}]: {e.args[1]}")
+        except IndexError:
+            print(f"MySQL Error: {str(e)}")
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
