6c0a56f5ce8944f76dfc0d49bd58ecc0f83eca46
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-04-11 22:51:09 +0200

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This script lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    databaseName = sys.argv[3]
+    search_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost', user=username, passwd=password, db=databaseName
+        )
+    cur = db.cursor()
+    try:
+        cur.execute(f"""SELECT * FROM states
+                    WHERE name LIKE BINARY '{search_name}'
+                    ORDER BY id""")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print(f"MySQL Error [{e.args[0]}]: {e.args[1]}")
+        except IndexError:
+            print(f"MySQL Error: {str(e)}")
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
