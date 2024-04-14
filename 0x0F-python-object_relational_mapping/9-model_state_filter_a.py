ea82485864c41d3fbaaa7c945c40642c7a31eb8a
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-04-14 13:28:43 +0200

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,6 +4,7 @@ the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import select, create_engine, text
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
@@ -13,10 +14,10 @@ if __name__ == '__main__':
         .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
         )
     Base.metadata.create_all(engine)
-    with engine.connect() as con:
-        s = select(State).order_by(State.id).filter(
-            State.name.like('%a%'))
-        result = con.execute(s)
-        rows = result.fetchall()
-        for row in rows:
-            print('{}: {}'.format(row.id, row.name))
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    s = session.query(State).order_by(State.id).filter(
+        State.name.like('%a%'))
+    for row in s:
+        print('{}: {}'.format(row.id, row.name))
