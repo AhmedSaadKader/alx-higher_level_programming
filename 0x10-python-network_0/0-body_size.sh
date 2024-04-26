364fce34d2936be8b0d2e653ca48e6aa6e9cddf7
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-04-26 20:59:08 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
-# A bash script that takes in a URL, sends a request to that URL, and displays the
-# size of the body of the response
+# A bash script that takes in a URL, sends a request and displays the size
 curl -s "$1" | wc -c
