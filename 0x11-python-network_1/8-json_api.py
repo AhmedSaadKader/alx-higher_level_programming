2392c99a3e1436f4ff79ca896734ec5374a93ae8
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-04-28 17:34:49 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,8 +9,11 @@ import requests
 if __name__ == "__main__":
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     r = requests.post('http://0.0.0.0:5000/search_user', data={"q": q})
-    res = r.json()
-    if not r:
-        print('No result')
-    else:
-        print('[{}] {}'.format(res['id'], res['name']))
+    try:
+        res = r.json()
+        if not r:
+            print('No result')
+        else:
+            print('[{}] {}'.format(res['id'], res['name']))
+    except ValueError:
+        print("Not a valid JSON")
